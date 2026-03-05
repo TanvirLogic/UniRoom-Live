18e68ff8684910d5d2de53160b2563b8f9a4610f
assignment <bmwthriad2023@gmail.com>
2026-03-05 11:06:06 +0600

    Initial commit

---
+++ lib/app/app.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class UniRoomLive extends StatefulWidget {
+  const UniRoomLive({super.key});
+
+  @override
+  State<UniRoomLive> createState() => _UniRoomLiveState();
+}
+
+class _UniRoomLiveState extends State<UniRoomLive> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp();
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/cupertino.dart';
+import 'package:uniroom_live/app/app.dart';
+
+void main() {
+  runApp(UniRoomLive());
+}
